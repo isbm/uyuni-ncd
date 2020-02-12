fbc7c239831de4d606a82922e7bedcee32625368
Bo Maryniuk <bo@suse.de>
2020-02-12 17:11:52 +0100

    add exit error codes to the runner interface

--- runners/runner.go
+++ runners/runner.go
@@ -4,6 +4,13 @@ import (
 	"github.com/isbm/uyuni-ncd/nanostate"
 )
 
+const (
+	ERR_OK = 0
+	ERR_FAILED = 1
+	ERR_TIMEOUT = 2 // Prepared, but unprocessed
+	ERR_INIT = 255
+)
+
 // Interface for the runner
 type Runner interface {
 	// Run the compiled and loaded nanostate
