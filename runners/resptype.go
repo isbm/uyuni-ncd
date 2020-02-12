6e02d2adf35e8d9ba9df9399b6afad4ca46f99e6
Bo Maryniuk <bo@suse.de>
2020-02-12 20:37:06 +0100

    Split response type into many objects for a nicer structure

--- runners/resptype.go
+++ runners/resptype.go
@@ -1,17 +1,35 @@
 package runners
 
+type RunnerStdResult struct {
+	Stdout  string
+	Stderr  string
+	Errmsg  string
+	Errcode int
+}
+
+type RunnerHostResult struct {
+	Host     string
+	Response map[string]RunnerStdResult
+}
+
+type RunnerResponseModule struct {
+	Module   string
+	Errcode  int
+	Errmsg   string
+	Response []RunnerHostResult
+}
+type RunnerResponseGroup struct {
+	Errcode  int
+	Errmsg   string
+	Response []RunnerResponseModule
+}
+
 type RunnerResponse struct {
 	Id          string
 	Description string
-	Groups      map[string][]struct {
-		Module   string
-		Errcode  int
-		Errmsg   string
-		Response []struct {
-			Results []struct {
-				Host     string
-				Response map[string]string
-			}
-		}
-	}
+	Groups      map[string]RunnerResponseGroup
+}
+
+func (rr *RunnerResponse) Serialise() map[interface{}]interface{} {
+	return nil
 }
