818621fa8a382fcbbf368012cf015d20f3a55fbc
Bo Maryniuk <bo@suse.de>
2020-02-13 20:52:32 +0100

    Rename transport lib

--- transport/ncdps.go
+++ transport/ncdps.go
@@ -2,7 +2,7 @@
 	Author: Bo Maryniuk
 	Node Controller daemon
 */
-package ncd
+package ncdtransport
 
 import (
 	"fmt"
@@ -17,31 +17,31 @@ type NatsURL struct {
 	Port   int
 }
 
-type Ncd struct {
+type NcdPubSub struct {
 	urls []*NatsURL
 	ncp  *nats.Conn
 	ncs  *nats.Conn
 }
 
-func NewNcd() *Ncd {
-	ncd := new(Ncd)
+func NewNcdPubSub() *NcdPubSub {
+	ncd := new(NcdPubSub)
 	ncd.urls = make([]*NatsURL, 0)
 	return ncd
 }
 
 // AddNatsServerURL adds NATS server URL to the cluster of servers to connect
-func (ncd *Ncd) AddNatsServerURL(host string, port int) *Ncd {
+func (ncd *NcdPubSub) AddNatsServerURL(host string, port int) *NcdPubSub {
 	ncd.urls = append(ncd.urls, &NatsURL{Scheme: "nats", Fqdn: host, Port: port})
 	return ncd
 }
 
 // IsConnected currently only indicates if the connection is initialised
-func (ncd *Ncd) IsConnected() bool {
+func (ncd *NcdPubSub) IsConnected() bool {
 	return ncd.ncp != nil && ncd.ncs != nil
 }
 
 // Format cluster URLs
-func (ncd *Ncd) getClusterURLs() string {
+func (ncd *NcdPubSub) getClusterURLs() string {
 	buff := make([]string, 0)
 	for _, nurl := range ncd.urls {
 		buff = append(buff, fmt.Sprintf("%s://%s:%d", nurl.Scheme, nurl.Fqdn, nurl.Port))
@@ -50,7 +50,7 @@ func (ncd *Ncd) getClusterURLs() string {
 }
 
 // Connect to the cluster
-func (ncd *Ncd) connect() {
+func (ncd *NcdPubSub) connect() {
 	var err error
 	log.Print("Connecting...")
 	if !ncd.IsConnected() {
@@ -68,7 +68,7 @@ func (ncd *Ncd) connect() {
 }
 
 // Disconnect from the cluster
-func (ncd *Ncd) Disconnect() {
+func (ncd *NcdPubSub) Disconnect() {
 	if ncd.IsConnected() {
 		log.Print("Begin disconnect")
 		for _, nc := range [2]*nats.Conn{ncd.ncp, ncd.ncs} {
@@ -83,15 +83,15 @@ func (ncd *Ncd) Disconnect() {
 	}
 }
 
-func (ncd *Ncd) GetPublisher() *nats.Conn {
+func (ncd *NcdPubSub) GetPublisher() *nats.Conn {
 	return ncd.ncp
 }
-func (ncd *Ncd) GetSubscriber() *nats.Conn {
+func (ncd *NcdPubSub) GetSubscriber() *nats.Conn {
 	return ncd.ncs
 }
 
 // Start starts the Node Controller
-func (ncd *Ncd) Start() {
-	log.Print("Starting ncd...")
+func (ncd *NcdPubSub) Start() {
+	log.Print("Starting ncd event listener...")
 	ncd.connect()
 }
